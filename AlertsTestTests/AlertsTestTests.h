1b99db4bcc0f49d962c55729ebb8117e9324f763
David Kapp <davekapp@gmail.com>
2012-01-29 22:58:11 -0700

    Initial Commit

---
+++ AlertsTestTests/AlertsTestTests.h
@@ -0,0 +1,13 @@
+//
+//  AlertsTestTests.h
+//  AlertsTestTests
+//
+//  Created by David Kapp on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AlertsTestTests : SenTestCase
+
+@end
---
+++ AlertsTestTests/AlertsTestTests.m
@@ -0,0 +1,32 @@
+//
+//  AlertsTestTests.m
+//  AlertsTestTests
+//
+//  Created by David Kapp on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AlertsTestTests.h"
+
+@implementation AlertsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AlertsTestTests");
+}
+
+@end
