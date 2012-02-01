d81e64bc79b0507f502501dbebca9d69a94d72da
David Kapp <davekapp@gmail.com>
2012-01-31 22:42:26 -0700

    all buttons operational

--- AlertsTest/ViewController.h
+++ AlertsTest/ViewController.h
@@ -7,7 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIAlertViewDelegate, UIActionSheetDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *output;
+
+- (IBAction)basicAlert:(id)sender;
+- (IBAction)alertWithButtons:(id)sender;
+- (IBAction)alertWithInput:(id)sender;
+- (IBAction)actionSheet:(id)sender;
+- (IBAction)playSound:(id)sender;
+- (IBAction)playAlertSound:(id)sender;
+- (IBAction)shakeItBaby:(id)sender;
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex;
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex;
 
 @end
--- AlertsTest/ViewController.m
+++ AlertsTest/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize output;
 
 - (void)didReceiveMemoryWarning
 {
@@ -26,6 +27,7 @@
 
 - (void)viewDidUnload
 {
+    [self setOutput:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -61,4 +63,103 @@
   }
 }
 
+- (IBAction)basicAlert:(id)sender {
+  UIAlertView *alertDialog;
+  alertDialog = [[UIAlertView alloc] 
+                 initWithTitle:@"This is an alert"
+                 message:@"Isn't that exciting"
+                 delegate:self
+                 cancelButtonTitle:@"Yeah whatever"
+                 otherButtonTitles:nil];
+  [alertDialog show];
+}
+
+- (IBAction)alertWithButtons:(id)sender {
+  UIAlertView *alertDialog;
+  alertDialog = [[UIAlertView alloc] initWithTitle:@"PRESS A BUTTON" message:@"You know you want to" delegate:self cancelButtonTitle:@"I refuse" otherButtonTitles:@"Ok", @"まあ、いいっか", nil];
+  [alertDialog show];
+}
+
+- (IBAction)alertWithInput:(id)sender {
+  UIAlertView *alertDialog = [[UIAlertView alloc]
+                              initWithTitle:@"Gimme text!"
+                              message:@"WAT UR EMAIL?"
+                              delegate:self
+                              cancelButtonTitle:@"NOES"
+                              otherButtonTitles:nil];
+  alertDialog.alertViewStyle = UIAlertViewStylePlainTextInput;
+  [alertDialog show];
+}
+
+- (IBAction)actionSheet:(id)sender {
+  UIActionSheet *sheet;
+  sheet = [[UIActionSheet alloc]
+           initWithTitle:@"OH SHEET"
+           delegate:self
+           cancelButtonTitle:@"Nuh-uh"
+           destructiveButtonTitle:@"BUUURN"
+           otherButtonTitles:@"Foo", @"Bar", @"Foobar", @"BIIIICOLOR", @"Mitted Mitted Mitted", @"OH YEAH", nil];
+  sheet.actionSheetStyle = UIActionSheetStyleBlackTranslucent;
+  [sheet showFromRect:[sender frame] inView:self.view animated:YES];
+}
+
+- (SystemSoundID)createSoundIdForCrash {
+  SystemSoundID soundId;
+  NSString *soundFile = [[NSBundle mainBundle] pathForResource:@"crash" ofType:@"wav"];
+  AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:soundFile], &soundId);
+  return soundId;
+}
+
+- (IBAction)playSound:(id)sender {
+  SystemSoundID soundId = [self createSoundIdForCrash];
+  AudioServicesPlaySystemSound(soundId);
+}
+
+- (IBAction)playAlertSound:(id)sender {
+  SystemSoundID soundId = [self createSoundIdForCrash];
+  AudioServicesPlayAlertSound(soundId);
+}
+
+- (IBAction)shakeItBaby:(id)sender {
+  AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+  NSString *clickedButtonTitle = [alertView buttonTitleAtIndex:buttonIndex];
+  
+  self.output.text = [NSString stringWithFormat:@"User selected: %@ at index %d", clickedButtonTitle, buttonIndex];
+
+//  NSString *inputText = [[alertView textFieldAtIndex:0] text];
+//  if (inputText) {
+//    self.output.text = [self.output.text stringByAppendingString:inputText];
+//  }
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+  NSString *clickedButtonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
+  self.output.text = [NSString stringWithFormat:@"User selected %@ at index %d", clickedButtonTitle, buttonIndex];
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
